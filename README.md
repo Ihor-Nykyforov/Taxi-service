f216ea67aff02f0dae059d910f1c6592099c101f
Ihor-Nykyforov <kifa1203@gmail.com>
2021-07-04 15:51:22 +0300

    Added README context

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-"# taxi-service" 
+# Taxi service 
+
+This is simple application of taxi service developed by using SOLID principles and N-tier architecture which has next functionality :
+
+- Register new driver and authorization
+- Create manufacturers and cars
+- Display all drivers, manufacturers and cars
+- Add car to driver
+- Display all cars by current driver
+
+##Technologies
+
+- JDBC
+- MySQL
+- Tomcat
+- Servlets
+- JSP, HTML, CSS
+- JSTL
+- Maven, Maven Checkstyle
+
+##Application setup
+
+- Clone this project to your local machine
+- Download and configure Apache Tomcat(recommend to use version 9.0.46)
+- Install MySQL
+- Using init.db to create schema and tables
+- Change constant values in ConnectionUtil class to your own
+- Run app
--- src/main/java/taxi/controller/IndexController.java
+++ src/main/java/taxi/controller/IndexController.java
@@ -5,11 +5,23 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
 
 public class IndexController extends HttpServlet {
+    private static final String DRIVER_ID = "driver_id";
+    private Injector injector = Injector.getInstance("taxi");
+    private DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Long id = (Long) session.getAttribute(DRIVER_ID);
+        Driver driver = driverService.get(id);
+        req.setAttribute("driver_name", driver.getName());
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
